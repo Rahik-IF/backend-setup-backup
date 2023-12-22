f3dc27f3c74243cfabb23a44bd845a6157f038dd
Rahik-IF <50521570+Rahik-IF@users.noreply.github.com>
2023-12-22 09:12:34 +0600

    full setup completed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,10 +5,10 @@
 //approach by promise
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => 
+    return (req, res, next) => 
     { Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error)) }
 } 
-
+export {asyncHandler};
 
 // approach by try catch
 /*
