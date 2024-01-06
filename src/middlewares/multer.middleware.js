3ce06d63c2c08fedd6706ebce22a2eed069d9a23
Rahik Ibne Forman <nehalhasan789@gmail.com>
2024-01-07 01:23:55 +0600

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
     //here we get req, and file. where i have access of req by nature. But file is not processed by express
     // so here we get the file, that's why multer is used with express
     destination: function (req, file, cb) {
-        cb(null, "../../public/temp")
+        cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
         const fileExt = path.extname(file.originalname);
